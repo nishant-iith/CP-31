3c3725632460a0a2efb02e01e9159a7c9902aee3
Nishant <iith.nishant@gmail.com>
2025-08-27 21:13:45 +0530

    Doremy_s_Paint_3

--- 800/A_Doremy_s_Paint_3.cpp
+++ 800/A_Doremy_s_Paint_3.cpp
@@ -13,24 +13,48 @@ typedef vector<int> vi;
 #define loop(i, a, b) for(int i=a; i<b; i++)
 #define rloop(i, a, b) for(int i=a-1; i>=b; i--)
 
-class Solution {
-    private:
-        vi arrayInput(){
+class Helper {
+    public:
+        static vi arrayInput(){
             int n;
             cin>>n;
             vi arr(n);
             loop(i, 0, n) cin>>arr[i];
             return arr;
         }
-        string stringInput(){
+        static string stringInput(){
             string s;
             cin>>s;
             return s;
         }
+};
 
+class Solution : public Helper {
     public:
         void solve(){
             // Your code here
+            vi arr = arrayInput();
+
+            map<ll, ll> mpp;
+            loop(i, 0, arr.size()){
+                mpp[arr[i]]++;
+            }
+
+            if(mpp.size()>2){
+                cout<<"No\n";
+            } else {
+                ll freq_1 = mpp.begin()->second;
+                ll freq_2 = mpp.rbegin()->second;
+
+                if(freq_1 == freq_2){
+                    cout<<"Yes\n";
+                }
+                else if (arr.size() % 2 == 1 && abs(freq_1 - freq_2) == 1){
+                    cout<<"Yes\n";
+                } else {
+                    cout<<"No\n";
+                }
+            }
         }
 };
 
