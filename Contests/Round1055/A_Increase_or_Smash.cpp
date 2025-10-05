64f9853bc6e983663e7bb34b0a80f67be1f06ddb
Nishant <iith.nishant@gmail.com>
2025-10-05 22:06:10 +0530

    Add solutions for "A. Increase or Smash" and "B. Catching the Krug" problems from Codeforces Round 1055

---
+++ Contests/Round1055/A_Increase_or_Smash.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define loop(i, a, b) for(int i=a; i<b; i++)
+#define rloop(i, a, b) for(int i=a-1; i>=b; i--)
+
+class Helper {
+protected:
+    vi arrayInput(){
+        int n;
+        cin>>n;
+        vi arr(n);
+        loop(i, 0, n) cin>>arr[i];
+        return arr;
+    }
+    vi arrayInput(int n){
+        vi arr(n);
+        loop(i, 0, n) cin>>arr[i];
+        return arr;
+    }
+    string stringInput(){
+        string s;
+        cin>>s;
+        return s;
+    }
+    void printArray(const vi &arr){
+        for(int i=0; i<arr.size(); i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+class Solution : public Helper {
+public:
+    void solve() {
+        int n; cin >> n;
+        static bool seen[101];
+        memset(seen, 0, sizeof(seen));
+        int m = 0;
+        for (int i = 0, x; i < n; ++i) {
+            cin >> x;
+            if (!seen[x]) {
+                seen[x] = true;
+                ++m;
+            }
+        }
+        cout << (2*m - 1) << '\n';
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        Solution sol;
+        sol.solve();
+    }
+    return 0;
+}
---
+++ Contests/Round1055/B_Catching_the_Krug.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define loop(i, a, b) for(int i=a; i<b; i++)
+#define rloop(i, a, b) for(int i=a-1; i>=b; i--)
+
+class Helper {
+protected:
+    vi arrayInput(){
+        int n;
+        cin>>n;
+        vi arr(n);
+        loop(i, 0, n) cin>>arr[i];
+        return arr;
+    }
+    vi arrayInput(int n){
+        vi arr(n);
+        loop(i, 0, n) cin>>arr[i];
+        return arr;
+    }
+    string stringInput(){
+        string s;
+        cin>>s;
+        return s;
+    }
+    void printArray(const vi &arr){
+        for(int i=0; i<arr.size(); i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+class Solution : public Helper {
+public:
+    void solve() {
+        long long n, rK, cK, rD, cD;
+        cin >> n >> rK >> cK >> rD >> cD;
+
+        long long dr = llabs(rK - rD);
+        long long dc = llabs(cK - cD);
+
+        long long rowPush = (rD > rK) ? rK : (rD < rK ? (n - rK) : 0);
+        long long colPush = (cD > cK) ? cK : (cD < cK ? (n - cK) : 0);
+
+        cout << max(dr + rowPush, dc + colPush) << '\n';
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        Solution sol;
+        sol.solve();
+    }
+    return 0;
+}
